6d3f513d43d7d7b272a6a8605536db7af896becd
Nikolas-Augustini <nikolas.augustini@gmail.com>
2026-07-09 16:44:43 -0600

    added proper self declaration.

--- rune-rush/scripts/hitbox.gd
+++ rune-rush/scripts/hitbox.gd
@@ -1,1 +1,25 @@
-extends Area2D
+class_name Hitbox extends Area2D
+
+#var attacker_stats: Stats add in later for a proper stats system
+
+var damage: int = 10 #temp damage for enemy
+
+var hitbox_lifetime: float
+var shape: Shape2D
+#TODO::add hitbox logging
+
+
+#setup initial values of the object
+func _init(damage: int, hitbox_lifetime: float, shape: Shape2D) -> void:
+	damage = damage
+	hitbox_lifetime = hitbox_lifetime
+	shape = shape
+
+#says that the hitbox will be detecting other shapes rather than being detected by other shapes
+func _ready() -> void:
+	monitorable = false
+	area_entered.connect(_on_area_entered)
+	
+#temporarily passes till we can detect a hurt box
+func _on_area_entered(area: Area2D) -> void:
+	pass
